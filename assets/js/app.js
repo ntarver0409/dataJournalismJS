43e990023833af29b96ecb366f405f4f1dcf25b3
ntarver0409 <nctarver4994@icloud.com>
2021-02-27 16:56:59 -0600

    Fixed filepath to data.csv, added axes to my chart.

--- assets/js/app.js
+++ assets/js/app.js
@@ -15,21 +15,52 @@ let height = svgHeight - margins.top - margins.bottom;
 
 //Making my svg wrapper 
 let svg = d3
-    .select("scatter")
+    .select(".scatter")
     .append("svg")
     .attr("width", svgWidth)
     .attr("height", svgHeight);
 
 //Appending a svg group to my html
 
-let chartGroup = svg.append("g");
+let chartGroup = svg.append("g")
+    .attr("transform", `translate(${margins.left}, ${margins.top})`);
 
 // Initial parameters 
 
 chosenXaxis = "income";
 
-d3.csv("./data/data.csv").then(function(stateData, err) {
+d3.csv("assets/data/data.csv").then(function(stateData, err) {
     if (err) throw err;
+
+    //parsing the data
+    stateData.forEach(function(data){
+        data.income = +data.income;
+        data.obesity = +data.obesity;
+        data.abbr = +data.abbr;
+    });
+
+    //Creating my scale functions
+    let xLinearScale = d3.scaleLinear()
+        .domain([20, d3.max(stateData, function(d){
+            return d.income;
+        })])
+        .range([0, width]);
+
+    let yLinearScale = d3.scaleLinear()
+    .domain([0, d3.max(stateData, function(d){
+        return d.obesity;
+    })])
+    .range([height, 0]);
+
+    //Appending Axes to the chart
+    chartGroup.append("g")
+        .attr("transform", `translate(0, ${height})`)
+        .call(bottomAxis);
+
+    chartGroup.append("g")
+        .call(leftAxis);
+
+    
 });
 
 
