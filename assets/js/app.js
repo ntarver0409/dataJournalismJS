d18fd6cdb6f2a938b2a158b42580baed5b754e4e
ntarver0409 <nctarver4994@icloud.com>
2021-02-27 16:19:30 -0600

    Initial code for my chart.

---
+++ assets/js/app.js
@@ -0,0 +1,17 @@
+// Setting up my svg dimensions
+let svgWidth = 960;
+let svgHeight = 500;
+
+//Setting up the margins
+let margins = {
+    top: 20,
+    right: 40,
+    bottom: 80,
+    left: 100
+};
+
+let width = svgWidth - margins.left - margins.right;
+let height = svgHeight - margins.top - margins.bottom;
+
+
+
