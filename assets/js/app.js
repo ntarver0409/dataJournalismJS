c5b65f831c14c5f63ff62ea9e2c813bb1bded850
ntarver0409 <nctarver4994@icloud.com>
2021-03-04 19:41:03 -0600

    Finished code to render chart in JS

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,21 +1,20 @@
 // Setting up my svg dimensions
-let svgWidth = 960;
+let svgWidth = 1000;
 let svgHeight = 500;
 
 //Setting up the margins
 let margins = {
     top: 20,
     right: 40,
-    bottom: 80,
-    left: 100
+    bottom: 60,
+    left: 80
 };
 
 let width = svgWidth - margins.left - margins.right;
 let height = svgHeight - margins.top - margins.bottom;
 
 //Making my svg wrapper 
-let svg = d3
-    .select(".scatter")
+let svg = d3.select("#scatter")
     .append("svg")
     .attr("width", svgWidth)
     .attr("height", svgHeight);
@@ -25,31 +24,29 @@ let svg = d3
 let chartGroup = svg.append("g")
     .attr("transform", `translate(${margins.left}, ${margins.top})`);
 
-// Initial parameters 
 
-chosenXaxis = "income";
+
 
 d3.csv("assets/data/data.csv").then(function(stateData, err) {
     if (err) throw err;
 
+    console.log(stateData);
     //parsing the data
     stateData.forEach(function(data){
-        data.income = +data.income;
-        data.obesity = +data.obesity;
-        data.abbr = +data.abbr;
+    data.poverty = +data.poverty;
+    data.healthcare= +data.healthcare;
     });
+
 
     //Creating my scale functions
     let xLinearScale = d3.scaleLinear()
-        .domain([20, d3.max(stateData, function(d){
-            return d.income;
-        })])
+        .domain([d3.min(stateData, d => d.poverty) * 0.5, 
+            d3.max(stateData, d => d.poverty) * 1.1])
         .range([0, width]);
 
     let yLinearScale = d3.scaleLinear()
-    .domain([0, d3.max(stateData, function(d){
-        return d.obesity;
-    })])
+    .domain([d3.min(stateData, d => d.healthcare) * 0.5, 
+        d3.max(stateData, d => d.healthcare) * 1.1])
     .range([height, 0]);
 
     //creating my axes
@@ -64,9 +61,55 @@ d3.csv("assets/data/data.csv").then(function(stateData, err) {
     chartGroup.append("g")
         .call(leftAxis);
 
+    //Creating circles for my chart
+    let circles = chartGroup.selectAll("circle")
+        .data(stateData)
+        .enter()
+        .append("circle")
+        .attr("cx", d => xLinearScale(d.poverty))
+        .attr("cy", d => yLinearScale(d.healthcare))
+        .attr("r", "15")
+        .classed("stateCircle", true);
 
+    console.log(stateData);
 
-    
+    // circles.append("text")
+    //     .text( d => d.abbr)
+    //     .attr("dx", d => xLinearScale(d.poverty))
+    //     .attr("dy", d => yLinearScale(d.healthcare))
+    //     .classed("stateText", true);
+
+        
+
+    let text = chartGroup.selectAll("g circles")
+        .data(stateData)
+        .enter()
+        .append("text")
+        .attr("dx", d => xLinearScale(d.poverty))
+        .attr("dy", d => yLinearScale(d.healthcare)+ 15/2.5)
+        .text(function(d) {
+            return d.abbr;
+        })
+        .classed("stateText", true);
+        // .attr("font-size", "10px")
+        // .attr("text-anchor", "middle")
+        // .attr("fill", "black");
+
+    // Creating axes labels
+    chartGroup.append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 0 - margins.left + 40)
+    .attr("x", 0 - (height / 2))
+    .attr("dy", "1em")
+    .attr("class", "axisText")
+    .text("Percentage with Healthcare");
+
+    chartGroup.append("text")
+    .attr("transform", `translate(${width / 2}, ${height + margins.top + 30})`)
+    .attr("class", "axisText")
+    .text("Percentage in Poverty");
+    }).catch(function(error) {
+    console.log(error);
 });
 
 
